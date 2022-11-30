9e4a2d2c8b335e79e218268f60d15837327c5b63
dangngocan01031999 <72962277+dangngocan01031999@users.noreply.github.com>
2022-11-30 18:59:46 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -58,7 +58,7 @@ EOF
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "//abc/$port/$(gen64 $IP6)"
+        echo "//dangngocan/$port/$(gen64 $IP6)"
     done
 }
 
@@ -89,7 +89,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
 FIRST_PORT=22000
-LAST_PORT=22700
+LAST_PORT=22200
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
